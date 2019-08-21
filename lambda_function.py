d0733c0caec166d0e5a7119ca7f1c167faa6173c
smitphilip <philipsmit@live.co.za>
2019-08-21 16:08:51 +0200

    amended to use one function for both starting and stopping instances

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,6 @@ ec2 = boto3.client('ec2', region_name=region)
 
 
 def lambda_handler(event, context):
-    
     deviceFilters=[
         {
             'Name': 'tag:DeviceCategory',
@@ -16,15 +15,17 @@ def lambda_handler(event, context):
                 ]
         },
     ]
-    
+
     instance_ids = []
     ec2Reservations = ec2.describe_instances(Filters=deviceFilters)['Reservations']
-    
+
     for reservation in ec2Reservations:
-          ec2Instances = reservation['Instances']
-          for instance in ec2Instances:
-              instance_ids.append(instance['InstanceId'])
-              
-    print("Starting instance: {}".format(','.join(instance_ids)))
-    ec2.start_instances(InstanceIds=instance_ids)
-    
+        ec2Instances = reservation['Instances']
+        for instance in ec2Instances:
+            instance_ids.append(instance['InstanceId'])
+            if instance['State']['Name'] == 'stopped':
+                print('starting instances')
+                ec2.start_instances(InstanceIds=instance_ids)
+            else:
+               print('stopping instances')
+               ec2.stop_instances(InstanceIds=instance_ids)
